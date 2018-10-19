4fa4c4d945a925c59a72f4e3bd0c50dfad2e6255
alexander zillion <alex@alexzillion.com>
2018-10-19 14:00:23 -0500

    Update tokentransport_test.go

--- registry/tokentransport_test.go
+++ registry/tokentransport_test.go
@@ -36,3 +36,32 @@ func TestErrBasicAuth(t *testing.T) {
 		t.Fatalf("expected empty token, got %v", err)
 	}
 }
+
+// func BenchmarkRoundTrip(t *testing.T) {
+// 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 		if r.URL.Path == "/" {
+// 			w.Header().Set("www-authenticate", `Basic realm="Registry Realm",service="Docker registry"`)
+// 			w.WriteHeader(http.StatusUnauthorized)
+// 		} else {
+// 			w.WriteHeader(http.StatusOK)
+// 		}
+// 	}))
+// 	defer ts.Close()
+
+// 	authConfig := types.AuthConfig{
+// 		Username:      "j3ss",
+// 		Password:      "ss3j",
+// 		ServerAddress: ts.URL,
+// 	}
+// 	r, err := New(authConfig.ServerAddress, authConfig, Opt{Insecure: true, Debug: true})
+// 	if err != nil {
+// 		t.Fatalf("expected no error creating client, got %v", err)
+// 	}
+// 	token, err := r.Token(ts.URL)
+// 	if err != ErrBasicAuth {
+// 		t.Fatalf("expected ErrBasicAuth getting token, got %v", err)
+// 	}
+// 	if token != "" {
+// 		t.Fatalf("expected empty token, got %v", err)
+// 	}
+// }
